c759c66072448b939e85762f773fd6b98a283cf4
jcanoy <jcanoy@techstyle.com>
2026-05-21 02:57:45 +0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import analytics
+from workers import WorkerEntrypoint  #
 
-app = FastAPI(title=" Portfolio API", version="1.0.0")
+app = FastAPI(title="Portfolio API", version="1.0.0")
 
 # Setup Routing modules
 app.include_router(analytics.router)
-#app.include_router(dashboard.router)
 
 # Global policies setup
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
+
+# ⚠️ ADD THIS CLASS AT THE BOTTOM OF THE FILE
+class Default(WorkerEntrypoint):
+    async def fetch(self, request, env, ctx): # type: ignore
+        return await app.fetch(request, env, ctx) # type: ignore
